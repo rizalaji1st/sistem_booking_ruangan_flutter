8fbe73264a476b60c179b59a3ed73238c0a34ae8
rizalaji1st <rizalaji561@gmail.com>
2021-06-24 07:27:20 +0700

    Menmbah view sementara pesan_ruang

--- lib/screens/pesan_ruang_screen.dart
+++ lib/screens/pesan_ruang_screen.dart
@@ -4,8 +4,6 @@ import 'package:provider/provider.dart';
 import '../models/ruang.dart';
 
 class PesanRuangScreen extends StatefulWidget {
-  
-
   static const routeName = '/pesan-ruang-screen';
 
   @override
@@ -13,10 +11,16 @@ class PesanRuangScreen extends StatefulWidget {
 }
 
 class _PesanRuangScreenState extends State<PesanRuangScreen> {
-  
+  DateTime tanggalPesan = DateTime.now();
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-
     final route = ModalRoute.of(context);
     if (route == null) return SizedBox.shrink();
     final ruangId = route.settings.arguments as String;
@@ -30,104 +34,131 @@ class _PesanRuangScreenState extends State<PesanRuangScreen> {
         child: SingleChildScrollView(
           child: Container(
             height: MediaQuery.of(context).size.height * .80,
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    Card(
-                      elevation: 4,
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * .80,
-                        height: MediaQuery.of(context).size.height * .20,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.spaceAround,
-                          children: [
-                            ListTile(
-                              title: Text(
-                                ruang.nama,
-                                style:
-                                    TextStyle(color: Colors.blue, fontSize: 20),
-                              ),
-                              subtitle:
-                                  Text('Kapasitas ${ruang.kapasitas} orang'),
-                            ),
-                            ElevatedButton(
-                              onPressed: () {},
-                              child: Container(
-                                decoration: BoxDecoration(
-                                    borderRadius: BorderRadius.circular(20),
-                                    color: Colors.blue),
-                                padding: EdgeInsets.symmetric(
-                                    vertical: 10, horizontal: 50),
-                                child: Text(
-                                  'Pesan Ruangan',
-                                  style: TextStyle(color: Colors.white),
-                                ),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Positioned(
-                      top: 4,
-                      right: 4,
-                      child: Container(
-                        padding: EdgeInsets.all(10),
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.only(
-                            bottomLeft: Radius.circular(15),
-                          ),
-                          color: Colors.blue,
-                        ),
-                        child: Row(
-                          children: [
-                            Text(
-                              ruang.biaya >= 100000
-                                  ? 'Rp. ${(ruang.biaya / 1000).round()} K '
-                                  : 'Rp. ${ruang.biaya} ',
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            Container(
-                                padding: EdgeInsets.all(3),
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(
-                                    Radius.circular(50),
-                                  ),
-                                  color: Colors.white,
-                                ),
-                                child: Text(
-                                  '/Jam',
-                                  style: TextStyle(
-                                      backgroundColor: Colors.white,
-                                      color: Colors.blue),
-                                )),
-                          ],
-                        ),
-                      ),
-                    ),
-                    Container(
-                      child: ElevatedButton(
-                        onPressed: () {},
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Stack(
+                    children: [
+                      Card(
+                        elevation: 4,
                         child: Container(
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(20),
-                              color: Colors.blue),
-                          padding: EdgeInsets.symmetric(
-                              vertical: 10, horizontal: 50),
-                          child: Text(
-                            'Pesan Ruangan',
-                            style: TextStyle(color: Colors.white),
+                          width: MediaQuery.of(context).size.width * .80,
+                          height: MediaQuery.of(context).size.height * .20,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              ListTile(
+                                title: Text(
+                                  ruang.nama,
+                                  style: TextStyle(
+                                      color: Colors.blue, fontSize: 20),
+                                ),
+                                subtitle:
+                                    Text('Kapasitas ${ruang.kapasitas} orang'),
+                              ),
+                              // ElevatedButton(
+                              //   onPressed: () {},
+                              //   child: Container(
+                              //     decoration: BoxDecoration(
+                              //         borderRadius: BorderRadius.circular(20),
+                              //         color: Colors.blue),
+                              //     padding: EdgeInsets.symmetric(
+                              //         vertical: 10, horizontal: 50),
+                              //     child: Text(
+                              //       'Pesan Ruangan',
+                              //       style: TextStyle(color: Colors.white),
+                              //     ),
+                              //   ),
+                              // ),
+                            ],
                           ),
                         ),
                       ),
-                    )
-                  ],
-                ),
-              ],
+                      Positioned(
+                        top: 4,
+                        right: 4,
+                        child: Container(
+                          padding: EdgeInsets.all(10),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(15),
+                            ),
+                            color: Colors.blue,
+                          ),
+                          child: Row(
+                            children: [
+                              Text(
+                                ruang.biaya >= 100000
+                                    ? 'Rp. ${(ruang.biaya / 1000).round()} K '
+                                    : 'Rp. ${ruang.biaya} ',
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Container(
+                                  padding: EdgeInsets.all(3),
+                                  decoration: BoxDecoration(
+                                    borderRadius: BorderRadius.all(
+                                      Radius.circular(50),
+                                    ),
+                                    color: Colors.white,
+                                  ),
+                                  child: Text(
+                                    '/Jam',
+                                    style: TextStyle(
+                                        backgroundColor: Colors.white,
+                                        color: Colors.blue),
+                                  )),
+                            ],
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Card(
+                    elevation: 4,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ListTile(
+                            title: Text(
+                              'Pilih Tanggal',
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 20),
+                            ),
+                          ),
+                          DatePickerDialog(
+                            initialDate: DateTime.now(),
+                            firstDate: DateTime(2020),
+                            lastDate: DateTime(2025),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Card(
+                    elevation: 4,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width * 0.8,
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          ListTile(
+                            title: Text(
+                              'Pilih Jam ',
+                              style:
+                                  TextStyle(color: Colors.blue, fontSize: 20),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
